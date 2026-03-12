70e1bf223dd1d1cbf927a7dca915c3fa0d96940f
amq-tutti <tutti-amq@proton.me>
2026-03-12 02:21:27 +0200

    Update title and description in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "claris-pr",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    title: "ClariS",
+    description: "Party rank sorter for ClariS anime songs."
 };
